f0e597c8e2014062910d1907c89c2834eff8323c
jomseni <jomseni642@gmail.com>
2022-07-04 11:17:54 +0900

    서비스란? 컨트롤러와 리퍼즈토리 사이에 위치 하는 계층으로 ! 처리업무순서 총괄한다 ex)  웨이터(컨트롤러)에게 주문들어오면 전달받은 셰프(서비스,요리총괄)이  필요한 재료를 보조 요리사(리퍼즈 토리)한테 가져오라한다! 리팩토링으로 코드를 간결 하게 만들 수 있다.
    클라이언트로부터  요청받은 컨트롤러는 요청받은것을 서비스에게 전달  서비스는 이를 받아 레시피에 따라 요리 , 필요한  재료는 리퍼즈토리가 가져온다(데이터베이스에서)
    
    트랜잭션이란?!
    모두성공되어야하는 일련의 과정!
    중간 단계에서 만약 실패한다면 처음으로 돌아가야한다! 처음으로 돌아가는 것을 RollBack이라고한다!

--- src/main/java/com/example/firstproject/api/ArticleApiController.java
+++ src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -3,6 +3,7 @@ package com.example.firstproject.api;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import com.example.firstproject.service.ArticleService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,64 +16,67 @@ import java.util.List;
 @RestController //RestApi 용 컨트롤러! 데이터(보통 JSON형태)를반환한다
 public class ArticleApiController {
 
-    @Autowired  //객체를 연결해주는 어노테이션 연결해준다!자유롭게 객체를 사용할 수있 게해준다, DI = 외부에서 가져온다는 뜻!(data.sql)
-    private ArticleRepository articleRepository; //객체
+    @Autowired  //객체를 연결해주는 어노테이션 연결해준다!자유롭게 객체를 사용할 수있 게해준다, DI = 외부에서 가져온다는 뜻!(data.sql),생성 객체를 가져와 연결!
+    private ArticleService articleService;
 
-    //GET메서드
+    //    private ArticleRepository articleRepository; //객체
+//
+    //GET메서드 (목록조회)
     @GetMapping("/api/articles")
-    public List<Article> index(){
-        return articleRepository.findAll();
+    public List<Article> index() {
+        return articleService.index();
     }
 
-
-    //단일 article 가져오기
+    //
+//
+    //단일 article 가져오기(단건 조회)
     @GetMapping("/api/articles/{id}")
-    public Article index(@PathVariable Long id){
-        return articleRepository.findById(id).orElse(null);
+    public Article show(@PathVariable Long id) {
+        return articleService.show(id);
     }
 
+    //
     //POST 새로 생성하는 요청!
     @PostMapping("/api/articles")
-    public Article create(@RequestBody ArticleForm dto){ //RestAPI에서 JSON으로 던질때는 @RequestBody가 있어야 받아진다!
-        Article article = dto.toEntity();
-        return articleRepository.save(article);
+    public ResponseEntity<Article> create(@RequestBody ArticleForm dto) { //RestAPI에서 JSON으로 던질때는 @RequestBody가 있어야 받아진다!
+        Article created = articleService.create(dto);
+        return (created != null) ?
+                ResponseEntity.status(HttpStatus.OK).body(created) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 
     }
+
     //PATCH
     @PatchMapping("/api/articles/{id}")
-    public ResponseEntity<Article> update(@PathVariable Long id, @RequestBody ArticleForm dto){ //상태코드를 같이 리턴하기위해 ResponseEntity<Article>로 바꿔준다.
+    public ResponseEntity<Article> update(@PathVariable Long id, @RequestBody ArticleForm dto) { //상태코드를 같이 리턴하기위해 ResponseEntity<Article>로 바꿔준다.
 
-        //1. 수정용 엔티티 생성
-        Article article = dto.toEntity();
-        log.info("id: {}, article: {}",id,article.toString()); //article은 객체 이므로 toString으로 캐스팅해준다!
-        //2. 대상 엔티티를 조회
-        Article target = articleRepository.findById(id).orElse(null);
-        //3. 잘못된 요청 처리(대상이 없거나,id가 다른경우)
-        if(target ==null || id != article.getId()){
-            //400 (잘못된 요청 응답!)
-            log.info("잘못된요청! id:{}, article:{}",id,article.toString());
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null); //BAD_REQUEST는 400번 에러를 반환하는 상태이다!
-        }
-        //4. 업데이트 및 정상 응답(200)
-        target.patch(article);
-        Article updated = articleRepository.save(target);
-        return ResponseEntity.status(HttpStatus.OK).body(updated);
+        Article updated = articleService.update(id, dto);  //주방장아 업데이트해줘~ 컨트롤러의 역할(웨이터)
+        return (updated != null) ?
+                ResponseEntity.status(HttpStatus.OK).body(updated) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 
-
-    //DELETE
-
+    //
+//
+//    //DELETE
+//      //웨이터가 요리까지 하는건 너무 복잡해!!!!!
+    //웨이터는 받고 반환만 하면된다!
     @DeleteMapping("/api/articles/{id}")
-    public ResponseEntity<Article> delete(@PathVariable Long id){
-        // 대상 찾기
-        Article target = articleRepository.findById(id).orElse(null);
-        //잘못된 요청 처리
-        if (target ==null){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        }
-        // 대상 삭제
-        articleRepository.delete(target);
-        // 데이터 반환
-        return ResponseEntity.status(HttpStatus.OK).build();
+    public ResponseEntity<Article> delete(@PathVariable Long id) {
+        //웨이터는 주문오고 반환하는거만 신경써!
+        Article deleted = articleService.delete(id); //요리는 주방장에 맡겨 서비스시켜, 서비스가 delete 함수해서 반환을 deleted로 반환할것이다!
+        //웨이터는 잘반환 된건지 아닌지만 판단하면된다.
+        return (deleted != null) ?
+                ResponseEntity.status(HttpStatus.NO_CONTENT).build():  //삭제가 잘된경우
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build(); //삭제가 잘못된경우 bad 보내줘
+    }
+
+    //트랜잭션(반드시 성공해야하는 일련의과정) -> 실패 -> 롤백!!!
+    @PostMapping("/api/transaction-test")
+    public ResponseEntity<List<Article>> transactionTest(@RequestBody List<ArticleForm> dtos){
+        List<Article> createdList =articleService.createArticles(dtos);
+        return (createdList != null) ?
+                ResponseEntity.status(HttpStatus.OK).body(createdList) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 }
---
+++ src/main/java/com/example/firstproject/service/ArticleService.java
@@ -0,0 +1,96 @@
+package com.example.firstproject.service;
+
+import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+//Service 부분은 모두 주방장의 일~
+@Service //서비스 선언!(서비스 객체를 스프링부트에 생성)
+@Slf4j //log 사용할 수 있게
+public class ArticleService {
+    //ArticleService가 Repository와 협업 할 수 있게 필드를 선언해준다!
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    //재료다가져와~
+    public List<Article> index() {
+       return articleRepository.findAll(); //요리사가 repository를 통해 데이터(재료)를 가져오게함!
+    }
+    //단건조회(재료몇개가져와~)
+    public Article show(Long id) { //보조요리사와 웨이터는 만날일이없어!!!!!!요리사가시키는거야!
+        return  articleRepository.findById(id).orElse(null);
+    }
+
+    //주방장의 메서드
+    public Article create(ArticleForm dto) {
+        Article article = dto.toEntity();
+        if(article.getId() != null){
+            return null;
+        }
+        return articleRepository.save(article);
+    }
+
+    public Article update(Long id, ArticleForm dto) {
+        //1. 수정용 엔티티 생성
+        Article article = dto.toEntity();
+        log.info("id: {}, article: {}",id, article.toString());
+
+        // 2. 대상 엔티티 찾기
+
+        Article target = articleRepository.findById(id).orElse(null);
+
+        //3 . 잘못된 요청 처리(대상이 없거나, id가 다른경우)
+        if(target == null || id != article.getId()){
+            log.info("잘못된 요청! id : {}, article: {}",id,article.toString());
+            return null;
+
+        }
+
+        //4: 업데이트
+        target.patch(article);///////////추가된코드
+        Article updated = articleRepository.save(target);//////변경된코드 (article-> target)
+        return updated;
+    }
+
+    public Article delete(Long id) {  //주방장은 요리주문취소됐으니깐 DB찾아서 지워
+        // 대상 찾기
+        Article target = articleRepository.findById(id).orElse(null);
+        //잘못된 요청 처리
+        if (target ==null){
+            return null;
+        }
+        // 대상 삭제
+        articleRepository.delete(target);
+        return target;
+    }
+
+
+    @Transactional //해당 메소드를 트랜잭션으로 묶는다! 트랜잭션은 서비스가 하는 행위이다
+    public List<Article> createArticles(List<ArticleForm> dtos) {
+        // dto 묶음을 entity 묶음으로 변환
+       List<Article> articleList = dtos.stream()
+                .map(dto -> dto.toEntity())
+                .collect(Collectors.toList());
+
+
+        // entity묶음을  db로 저장
+        articleList.stream()
+                .forEach(article -> articleRepository.save(article));
+
+        // 강제로 예의 발생
+        articleRepository.findById(-1L).orElseThrow(
+                () -> new IllegalArgumentException("결제 실패!")
+        );
+
+        //결과값 반환
+        return null;
+    }
+}
